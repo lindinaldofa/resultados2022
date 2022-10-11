18e554f1c7fc3110955aae4206922ec66c6e3026
Lindinaldo Freitas de Alencar <lindinaldo.alencar@gmail.com>
2022-10-10 23:10:22 -0300

    Dados via hospedagem

--- _dados.py
+++ _dados.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 
 #importando a base de dados
-data22 = pd.read_csv('assets/dataeleicao22.csv')
+data22 = pd.read_csv('https://pcdobalagoas.org.br/assets/dataeleicao22.csv')
 data22.rename(columns={'NM_MUNICIPIO':'Cidade', 'QT_VOTOS':'Votos'}, inplace=True)
 
 #gerando lista de nomes
-nomes = list(data22.NM_VOTAVEL.unique())
+nomes = list(data22['NM_VOTAVEL'].unique())
 #gerando lista de cargos
 cargos = list(data22['DS_CARGO'].unique())
 
